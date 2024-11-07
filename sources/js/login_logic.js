ccfebe4ace84f2298399e6a6443f3f62fe11b431
Jonas <riemer.jonas@outlook.de>
2024-11-07 09:25:29 +0100

    Started with login page

---
+++ sources/js/login_logic.js
@@ -0,0 +1,11 @@
+
+function onLogin()
+{
+    var mail        = document.getElementById("input_login_email");
+    var password    = document.getElementById("input_login_password");
+
+    var request_body = {
+        
+    };
+
+}
