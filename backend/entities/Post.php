ff718aabc5a7d610a7cb86e3e3623db68087ed27
Jonas <riemer.jonas@outlook.de>
2024-11-12 13:35:05 +0100

    Add Post entity class; define structure for user relationships and friend requests

---
+++ backend/entities/Post.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  =================================================================================
+ *  Name        :       Relationship.php
+ *  Purpose     :       Entity class for the relationships between users (FriendRequests)
+ *  Authors     :       Jonas Riemer, Fabian Belli
+ *  Last edited :       07.11.2024
+ *  =================================================================================
+ *  
+ *  USAGE       :
+ *  Include this file in your PHP script to get access to the Relationsship class.
+ *  
+ *  EXAMPLE     :
+ *  $user = new UserProfile(array());
+ *  $user->save();
+ *  $user->delete();
+ *  $users = UserProfile::getAll();
+ *  $user = UserProfile::findByID('user_id');
+ *  $user = UserProfile::findByEmail('email');
+ *  
+ */
+namespace entities;
+class Post
+{
+
+}
+?>
