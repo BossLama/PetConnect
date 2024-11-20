67dd2dc7de8d293af6b25b6ee7156d9c6bb5dcea
Jonas <riemer.jonas@outlook.de>
2024-11-20 18:37:44 +0100

    Implement post loading functionality: add PostLoadController for fetching and rendering posts with automatic updates

---
+++ sources/js/controller/PostLoadController.js
@@ -0,0 +1,95 @@
+class PostLoadController
+{
+    lastIndex       = 0;
+    limit           = 20;
+
+    loadPosts(fromIndex = 0)
+    {
+
+        fetch(API_URL + "?endpoint_id=post&min=" + fromIndex + "&limit=" + this.limit, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": profileManager.getAuthToken()
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if(data.status == "success")
+            {
+                data.data.forEach(post => {
+                    this.renderPost(post);
+                });
+                this.lastIndex = fromIndex + data.data.length;
+            }
+            else
+            {
+                console.error(data.message);
+                console.log(data);
+            }
+        })
+    }
+
+
+    renderPost(post)
+    {
+        var username        = post.creator.username;
+        var posted_at       = post.posted_at;
+        var visibility      = post.visibility;
+        var message         = post.message;
+
+        if (visibility == "0") visibility = "Privat";
+        if (visibility == "1") visibility = "Freunde";
+        if (visibility == "2") visibility = "Kommune";
+        if (visibility == "3") visibility = "Öffentlich";
+
+        var posted_at = new Date(posted_at);
+        var now = new Date();
+        var timeDifference = now - posted_at;
+        var timeDifferenceInHours = timeDifference / 1000 / 60 / 60;
+        var timeDifferenceInMinutes = timeDifference / 1000 / 60;
+
+        var timeString = "";
+        if (timeDifferenceInHours > 24) timeString = "vor " + Math.floor(timeDifferenceInHours / 24) + " Tagen";
+        if (timeDifferenceInHours < 24) timeString = "vor " + Math.floor(timeDifferenceInHours) + " Stunden";
+        if (timeDifferenceInMinutes < 60) timeString = "vor " + Math.floor(timeDifferenceInMinutes) + " Minuten";
+        if (timeDifferenceInMinutes < 1) timeString = "vor wenigen Sekunden";
+        
+
+        var postElement = document.createElement("article");
+        postElement.dataset.post_id = post.post_id;
+        postElement.className = "post";
+
+        postElement.innerHTML = `
+            <div class="header">
+                <img src="resources/placeholder/plaho_profile_dog.png" alt="Profile">
+                <p class="username">`+ username +`</p>
+                <p class="posted">`+ timeString +` - `+ visibility +`</p>
+            </div>
+            <div class="message">`+ message +`</div>
+            <div class="controlls">
+                <button class="button-controll"><img src="resources/icons/icon_light_favorite.svg" alt="Like"></button>
+                <button class="button-controll"><img src="resources/icons/icon_light_comment.svg" alt="Comment"></button>
+            </div>`;
+
+        // Insert the post at the beginning of the post container
+        var postContainer = document.getElementById("feed");
+        var topScroll = postContainer.scrollTop;
+        postContainer.insertBefore(postElement, postContainer.firstChild);
+
+        // If feed scroll < 100px from top, scroll to top
+        if (topScroll < 100) postContainer.scrollTop = 0;
+
+    }
+}
+
+var postLoadController = null;
+document.addEventListener('DOMContentLoaded', function() {
+    postLoadController = new PostLoadController();
+    postLoadController.loadPosts(0);
+
+    setInterval(() => {
+        postLoadController.loadPosts(postLoadController.lastIndex);
+    }, 1000);
+});
