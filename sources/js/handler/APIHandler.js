fb3fcbbed7e9057fa3506aabfad60bbec53fdb56
Jonas <riemer.jonas@outlook.de>
2024-11-29 12:41:23 +0100

    Add blur view overlay for mobile navigation toggle and enhance button functionality

--- sources/js/handler/APIHandler.js
+++ sources/js/handler/APIHandler.js
@@ -10,6 +10,17 @@ function createMobileButton()
 {
     if(window.location.pathname.includes('login.html')) return;
 
+    var blurView = document.createElement("div");
+    blurView.classList.add("blur-view");
+    blurView.classList.add("hidden");
+    document.body.appendChild(blurView);
+
+    blurView.addEventListener("click", function() {
+        var sideNav = document.querySelector(".side-nav");
+        sideNav.classList.remove("mobile-show");
+        blurView.classList.add("hidden");
+    });
+
     var button = document.createElement("button");
     button.classList.add("side-nav-mobile-toggle");
     button.innerHTML = '<img src="./resources/icons/icon_light_menu.svg" alt="Menu">';
@@ -17,6 +28,7 @@ function createMobileButton()
     button.addEventListener("click", function() {
         var sideNav = document.querySelector(".side-nav");
         sideNav.classList.toggle("mobile-show");
+        blurView.classList.toggle("hidden");
     });
 
     document.body.appendChild(button);
