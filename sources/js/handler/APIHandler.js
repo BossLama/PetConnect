00ce5dca47fac9686fa0928a502ce7c5e8a357dd
Jonas <riemer.jonas@outlook.de>
2024-11-29 11:35:49 +0100

    Add mobile toggle button for side navigation and style adjustments

--- sources/js/handler/APIHandler.js
+++ sources/js/handler/APIHandler.js
@@ -1,1 +1,21 @@
 const API_URL = "./backend/index.php";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    createMobileButton();
+});
+
+
+function createMobileButton()
+{
+    var button = document.createElement("button");
+    button.classList.add("side-nav-mobile-toggle");
+    button.innerHTML = '<img src="resources/icons/icon_light_menu.svg" alt="Menu">';
+
+    button.addEventListener("click", function() {
+        var sideNav = document.querySelector(".side-nav");
+        sideNav.classList.toggle("mobile-show");
+    });
+
+    document.body.appendChild(button);
+}
