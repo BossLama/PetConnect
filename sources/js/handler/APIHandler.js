93c0e80900a92ef49db7bd42b4b8fe1ff5bd47e8
Jonas <riemer.jonas@outlook.de>
2024-11-29 12:18:33 +0100

    Refactor mobile toggle button styles and relocate to mod_default.css; update image path in APIHandler.js

--- sources/js/handler/APIHandler.js
+++ sources/js/handler/APIHandler.js
@@ -10,7 +10,7 @@ function createMobileButton()
 {
     var button = document.createElement("button");
     button.classList.add("side-nav-mobile-toggle");
-    button.innerHTML = '<img src="resources/icons/icon_light_menu.svg" alt="Menu">';
+    button.innerHTML = '<img src="./resources/icons/icon_light_menu.svg" alt="Menu">';
 
     button.addEventListener("click", function() {
         var sideNav = document.querySelector(".side-nav");
